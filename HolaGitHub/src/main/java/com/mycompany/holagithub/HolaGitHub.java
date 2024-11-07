da335a5176d86e96d87ece11c855630281ab5ace
Honorato Saavedra Hernández <honoratosaavedra@gmail.com>
2024-11-07 08:00:28 -0600

    Se agregó un hola mundo

--- HolaGitHub/src/main/java/com/mycompany/holagithub/HolaGitHub.java
+++ HolaGitHub/src/main/java/com/mycompany/holagithub/HolaGitHub.java
@@ -12,5 +12,7 @@ public class HolaGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        System.out.println("Hello World!");
+        
     }
 }
