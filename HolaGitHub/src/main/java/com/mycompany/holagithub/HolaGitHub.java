384dfdaaba41eb1ce45f87dbd9e26599a57d39b3
Honorato Saavedra Hernández <honoratosaavedra@gmail.com>
2024-11-07 07:53:38 -0600

    versión inicial

---
+++ HolaGitHub/src/main/java/com/mycompany/holagithub/HolaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holagithub;
+
+/**
+ *
+ * @author honorato
+ */
+public class HolaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
